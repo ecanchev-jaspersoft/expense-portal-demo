b6284b71bd69cb4d96a53f942569d4a20a35b416
Eduardo Canche Vazquez <ecanchev@tibco.com>
2025-10-13 16:02:05 -0600

    Integration added with Visualize.js

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,37 @@
+import { useEffect } from 'react';
 import './Dashboard.css';
+import './viz.css';
 
 const Dashboard = () => {
+  useEffect(() => {
+    if (!window.visualize) {
+      console.error("visualize.js library is not loaded.");
+      return;
+    }
+    window.visualize({
+      auth: {
+        name: "joeuser",
+        password: "joeuser",
+        organization: "organization_1"
+      }
+    }, function (v) {
+      v("#viz-container").adhocView({
+        resource: "/public/viz/Adhoc/Change_Visualization1",
+        error: function (e) {
+          console.log(e);
+        }
+      });
+    });
+  }, []);
+
   return (
     <div className="dashboard-page h-main-section">
       <h1>Welcome to the Dashboard</h1>
-      <p>This is a mock dashboard page for logged-in users.</p>
+
+      {/* Provide container to render your visualization */}
+      <div id="viz-container"></div>
     </div>
   );
-}
+};
 
 export default Dashboard;
