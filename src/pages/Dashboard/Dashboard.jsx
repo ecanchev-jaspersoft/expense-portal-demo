6ffd88d039d90143bbc8eb65d39cb79b8dc4b585
Eduardo Canche Vazquez <ecanchev@tibco.com>
2025-10-14 13:54:21 -0600

    sidebar added to easily switch among different visualizations

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,35 +1,72 @@
-import { useEffect } from 'react';
-import './Dashboard.css';
-import './viz.css';
+import { useEffect, useState } from "react";
+import "./Dashboard.css";
+import "./viz.css";
+
+const charts = [
+  {
+    name: "Expenses By Categories and Payment Methods",
+    resource: "/public/Samples/Ad_Hoc_Views/VIZ/ExpensesOverTime",
+  },
+  {
+    name: "Expenses By Date",
+    resource: "/public/Samples/Ad_Hoc_Views/VIZ/ExpensesByDate",
+  },
+  {
+    name: "Expenses By Department And Approval Status",
+    resource:
+      "/public/Samples/Ad_Hoc_Views/VIZ/ExpensesByDepartmentAndApprovalStatus",
+  },
+];
 
 const Dashboard = () => {
+  const [selectedChart, setSelectedChart] = useState(charts[0].resource);
+
   useEffect(() => {
     if (!window.visualize) {
       console.error("visualize.js library is not loaded.");
       return;
     }
-    window.visualize({
-      auth: {
-        name: "superuser",
-        password: "superuser"
+    window.visualize(
+      {
+        auth: {
+          name: "superuser",
+          password: "superuser",
+        },
+      },
+      (v) => {
+        v("#viz-container").adhocView({
+          resource: selectedChart,
+          error: (e) => {
+            console.log(e);
+          },
+        });
       }
-    }, function (v) {
-      v("#viz-container").adhocView({
-        resource: "/public/Samples/Ad_Hoc_Views/VIZ/ExpensesOverTime",
-        error: function (e) {
-          console.log(e);
-        }
-      });
-    });
-  }, []);
+    );
+  }, [selectedChart]);
 
   return (
-    <div className="dashboard-page h-main-section">
-      <h1>Welcome to the Dashboard</h1>
+    <main className="dashboard-page h-main-section">
+      <section className="sidebar">
+        <h2>Charts</h2>
+        <ul>
+          {charts.map((chart) => (
+            <li
+              key={chart.resource}
+              className={chart.resource === selectedChart ? "active" : ""}
+              onClick={() => setSelectedChart(chart.resource)}
+            >
+              {chart.name}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className="main-content">
+        <h1>Welcome to the Dashboard</h1>
 
-      {/* Provide container to render your visualization */}
-      <div id="viz-container"></div>
-    </div>
+        {/* Provide container to render your visualization */}
+        <div id="viz-container"></div>
+      </section>
+    </main>
   );
 };
 
